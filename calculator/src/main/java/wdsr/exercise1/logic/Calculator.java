c2361058f47539c782de0955013b3d3aaaff4bab
matejablonski <mateusz1812@gmail.com>
2017-07-05 23:31:52 +0100

    Added tests and corrected max function

--- calculator/src/main/java/wdsr/exercise1/logic/Calculator.java
+++ calculator/src/main/java/wdsr/exercise1/logic/Calculator.java
@@ -6,20 +6,23 @@ import org.apache.commons.lang3.math.NumberUtils;
  * Created by Marek on 14.02.2016.
  */
 public class Calculator {
-    public int min(int... values) {
-        return NumberUtils.min(values);
-    }
+  public int min(int... values) {
+    return NumberUtils.min(values);
+  }
 
-    public int max(int... values) {
-        if (values == null || values.length == 0) {
-        	throw new IllegalArgumentException("Null or empty argument: "+values);
-        };
-        int result = 0;
-        for (int value: values) {
-        	if (value > result) {
-        		result = value;
-        	}
+  public int max(int... values) {
+    if (values == null || values.length == 0) {
+      throw new IllegalArgumentException("Null or empty argument: " + values);
+    } ;
+    int result = 0;
+    if (values.length == 1)
+      result = values[0];
+    else
+      for (int value : values) {
+        if (value > result) {
+          result = value;
         }
-        return result;
-    }
+      }
+    return result;
+  }
 }
--- calculator/src/test/java/wdsr/exercise1/logic/CalculatorTest.java
+++ calculator/src/test/java/wdsr/exercise1/logic/CalculatorTest.java
@@ -1,63 +1,113 @@
 package wdsr.exercise1.logic;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class CalculatorTest {
-	private Calculator calculator;
+  private Calculator calculator;
 
-	@Before
-	public void setup() {
-		calculator = new Calculator();
-	}
-	
-	@Test
-	public void testMin_shouldReturnTheOnlyValue() {
-		// given
-		int[] values = { -5 };
-		
-		// when
-		int min = calculator.min(values);
-		
-		// then
-		assertEquals(values[0], min);
-	}
-	
-	@Test
-	public void testMin_shouldReturnTheSmallestOfAllValues() {
-		// given
-		int[] values = { 2, -3, 5 };
-		
-		// when
-		int min = calculator.min(values);
-		
-		// then
-		assertEquals(-3, min);
-	}
-	
-	@Test(expected=IllegalArgumentException.class)
-	public void testMin_shouldRaiseAnExceptionForNullArgument() {
-		// given
-		int[] values = null;
-		
-		// when
-		calculator.min(values);
-		
-		// then
-		// empty
-	}
-	
-	@Test(expected=IllegalArgumentException.class)
-	public void testMin_shouldRaiseAnExceptionForEmptyArgument() {
-		// given
-		int[] values = {};
-		
-		// when
-		calculator.min(values);
-		
-		// then
-		// empty
-	}		
+  @Before
+  public void setup() {
+    calculator = new Calculator();
+  }
+
+  @Test
+  public void testMin_shouldReturnTheOnlyValue() {
+    // given
+    int[] values = {-5};
+
+    // when
+    int min = calculator.min(values);
+
+    // then
+    assertEquals(values[0], min);
+  }
+
+  @Test
+  public void testMin_shouldReturnTheSmallestOfAllValues() {
+    // given
+    int[] values = {2, -3, 5};
+
+    // when
+    int min = calculator.min(values);
+
+    // then
+    assertEquals(-3, min);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testMin_shouldRaiseAnExceptionForNullArgument() {
+    // given
+    int[] values = null;
+
+    // when
+    calculator.min(values);
+
+    // then
+    // empty
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testMin_shouldRaiseAnExceptionForEmptyArgument() {
+    // given
+    int[] values = {};
+
+    // when
+    calculator.min(values);
+
+    // then
+    // empty
+  }
+
+  @Test
+  public void testMax_shouldReturnTheOnlyValue() {
+    // given
+    int[] values = {-5};
+
+    // when
+    int min = calculator.max(values);
+
+    // then
+    assertThat(values[0], is(min));
+  }
+
+  @Test
+  public void testMax_shouldReturnTheLargestOfAllValues() {
+    // given
+    int[] values = {2, -3, 5};
+
+    // when
+    int max = calculator.max(values);
+
+    // then
+    assertThat(5, is(max));
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testMax_shouldRaiseAnExceptionForNullArgument() {
+    // given
+    int[] values = null;
+
+    // when
+    calculator.max(values);
+
+    // then
+    // empty
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testMax_shouldRaiseAnExceptionForEmptyArgument() {
+    // given
+    int[] values = {};
+
+    // when
+    calculator.max(values);
+
+    // then
+    // empty
+  }
 }
